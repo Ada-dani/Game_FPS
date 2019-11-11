9b62b96facded139b24eaa5c53766785397aedd5
Ada-dani <omar1.ourahou@gmail.com>
2019-11-11 13:23:54 +0100

    Add files via upload

---
+++ FPS/Assets/ChooseArm.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseArm : MonoBehaviour {
+
+    public GameObject Arm1;
+    public GameObject Arm2;
+    public int NrArm = 1;
+    public AudioClip SwitchArm;
+
+    // Use this for initialization
+    void Start()
+    {
+        Arm1.SetActive(true);
+        Arm2.SetActive(false);
+
+
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            NrArm = 1;
+            //Arm2.GetComponent<Animator>().SetTrigger("Descent");
+            StartCoroutine(Pause());
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+            NrArm = 2;
+            //Arm1.GetComponent<Animator>().SetTrigger("Descent");
+            StartCoroutine(Pause());
+        }
+
+    }
+
+    IEnumerator Pause()
+    {
+        GetComponent<AudioSource>().PlayOneShot(SwitchArm);
+
+        if (NrArm == 2)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Arm1.SetActive(false);//deactivate arm1
+            Arm2.SetActive(true);//active arm2
+            Arm2.GetComponent<Animator>().SetTrigger("PlayerIdle");//animation playeridle
+
+        }
+
+        if (NrArm == 1)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Arm2.SetActive(false);//deactivate arm2
+            Arm1.SetActive(true);//active arm1
+            Arm1.GetComponent<Animator>().SetTrigger("PlayerIdle");//animation playeridle
+
+        }
+    }
+}
